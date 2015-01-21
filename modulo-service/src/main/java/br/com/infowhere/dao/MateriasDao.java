cb18c87f792c3a28ea33f6cfbd0ea300b3820f4c
ltsiciliano <ltsiciliano@gmail.com>
2015-01-21 21:01:29 -0200

    Anotações para injeção de dependência

--- modulo-service/src/main/java/br/com/infowhere/dao/MateriasDao.java
+++ modulo-service/src/main/java/br/com/infowhere/dao/MateriasDao.java
@@ -4,7 +4,9 @@ package br.com.infowhere.dao;
 import br.com.infowhere.crud.CrudService;
 import br.com.infowhere.dao.interfaces.ImateriasDao;
 import br.com.infowhere.model.Materia;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class MateriasDao extends CrudService<Materia> implements ImateriasDao {
 
 }
